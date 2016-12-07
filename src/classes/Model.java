c23e25a645db32ecd3670b3b4c7a3c26c7500344
usainzg <unaihtc70@gmail.com>
2016-12-07 10:41:29 +0100

    Model class added with FTP server necessary data.

---
+++ src/classes/Model.java
@@ -0,0 +1,12 @@
+package classes;
+
+public class Model {
+	
+	// SERVER DATA
+	public static final String FTP_SERVER_ADDR = "192.168.56.101";
+	public static final int FTP_CONTROL_PORT = 21;
+	public static final int FTP_DATA_PORT = 20;
+	public static final String FTP_SERVER_USER = "usainzg";
+	public static final String FTP_SERVER_PASSWORD = "serv1doR";
+
+}
