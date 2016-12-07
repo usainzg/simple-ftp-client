e29a027ab229bf5774a9e35714dfad2a14a15a59
usainzg <unaihtc70@gmail.com>
2016-12-07 11:48:52 +0100

    Main created and model edited.

--- src/classes/Model.java
+++ src/classes/Model.java
@@ -7,6 +7,7 @@ public class Model {
 	public static final int FTP_CONTROL_PORT = 21;
 	public static final int FTP_DATA_PORT = 20;
 	public static final String FTP_SERVER_USER = "usainzg";
-	public static final String FTP_SERVER_PASSWORD = "serv1doR";
+	public static final String FTP_SERVER_PASSWORD = "mofeta";
+	public static final String FTP_SERVER_ACCESS_ADDR = FTP_SERVER_USER + "@" + FTP_SERVER_ADDR;
 
 }
